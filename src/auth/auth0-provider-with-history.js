88679b4d7b6c49bbd3605a8353a05e6d7069bea3
dkruggel <david.kruggel@gmail.com>
2020-12-17 20:16:05 -0600

    auth

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -3,8 +3,8 @@ import { useHistory } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
   const history = useHistory();
 
@@ -14,9 +14,12 @@ const Auth0ProviderWithHistory = ({ children }) => {
 
   return (
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={'https://alfredo-recommends.tk'}
+      // domain={domain}
+      // clientId={clientId}
+      // redirectUri={'https://alfredo-recommends.tk'}
+      domain="alfredo-recommender.us.auth0.com"
+      clientId="IBylyr7Ssb82rggjHjsfiRuY7Pvwsaue"
+      redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
