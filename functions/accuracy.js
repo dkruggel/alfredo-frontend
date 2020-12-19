c390d3c943978b8973f259a70703f97029df1fd0
dkruggel <david.kruggel@gmail.com>
2020-12-19 09:25:23 -0600

    s

--- functions/accuracy.js
+++ functions/accuracy.js
@@ -4,7 +4,7 @@ exports.handler = function (event, context, callback) {
   return new Promise(function (success, nosuccess) {
     const python = spawn('/usr/bin/python3', [
       './algo/main.py',
-      context.user,
+      'ofKDkJKXSKZXu5xJNGiiBQ',
       'True',
     ]);
 
